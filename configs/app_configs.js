049236bd1e9b291533f88378548135433a415bb1
ntthuan060102github <ntthuan060102.work@gmail.com>
2023-01-06 11:40:07 +0700

    re-config env var

--- configs/app_configs.js
+++ configs/app_configs.js
@@ -1,6 +1,6 @@
 const ROUTES_PREFIX = `/selina-bookshelves-api`
 const SECRET_KEY = process.env.SECRET_KEY
-const APP_ENV = process.env.app_env
+const APP_ENV = process.env.APP_ENV
 const REDIS_ENDPOINT_URI = process.env.REDIS_ENDPOINT_URI
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD
 const MONGO_DB_URL = process.env.MONGO_DB_URL
